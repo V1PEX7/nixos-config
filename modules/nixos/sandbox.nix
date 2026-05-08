f3729e35bd51565fe4a1c047d417f8222f96f338
xnp <micbel2000@gmail.com>
2026-05-09 01:47:47 +0300

    apparmor enabled

--- modules/nixos/sandbox.nix
+++ modules/nixos/sandbox.nix
@@ -11,11 +11,11 @@ in
   options.modules.sandbox.enable = lib.mkEnableOption "system-level sandboxing support (AppArmor, bwrap)";
 
   config = lib.mkIf cfg.enable {
-    # security.apparmor = {
-    #   enable = true;
-    #   killUnconfinedConfinables = true;
-    #   packages = [ pkgs.apparmor-profiles ];
-    # };
+    security.apparmor = {
+      enable = true;
+      killUnconfinedConfinables = true;
+      packages = [ pkgs.apparmor-profiles ];
+    };
 
     environment.systemPackages = with pkgs; [
       bubblewrap
