e227ef4597311c6fc86ad1ac06dfd32daf562cca
xnp <>
2026-05-19 23:02:19 +0300

    networking: disabled llmnr and mdns for resolved

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -19,6 +19,8 @@ in
         DNS = "9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net 2620:fe::fe#dns.quad9.net 2620:fe::9#dns.quad9.net";
         DNSOverTLS = "true";
         DNSSEC = "true";
+        llmnr = "false";
+        mdns = "false";
         Domains = [ "~." ];
         FallbackDNS = "";
       };
