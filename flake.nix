f3dcbada2945add569758acba2ab6cc3076d024c
xnp <>
2026-05-25 01:56:25 +0300

    flake: now using nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration (desktop + laptop)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     home-manager = {
       url = "github:nix-community/home-manager";
