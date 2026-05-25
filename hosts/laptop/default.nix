7c8dbc7796125a0cb8884e5a4c52db066482607b
xnp <>
2026-05-25 18:18:03 +0300

    removed unused udev rule

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -10,12 +10,6 @@
 
   services.power-profiles-daemon.enable = true;
 
-  services.udev.extraRules = ''
-    SUBSYSTEM=="power_supply", KERNEL=="BAT0", \
-      RUN+="${pkgs.coreutils}/bin/chgrp battery_ctl /sys%p/charge_control_end_threshold", \
-      RUN+="${pkgs.coreutils}/bin/chmod g+w /sys%p/charge_control_end_threshold"
-  '';
-
   services.getty.autologinUser = "xnp";
 
   modules = {
