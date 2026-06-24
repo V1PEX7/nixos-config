94bf4a41115c85293b637d5c791e6dc3d5426634
xnp <xnppp@protonmail.com>
2026-06-24 03:06:28 +0300

    nix: now using nh

--- hosts/common/nix.nix
+++ hosts/common/nix.nix
@@ -5,14 +5,17 @@
     "flakes"
   ];
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-
   nix.optimise = {
     automatic = true;
     dates = [ "weekly" ];
+  };
+
+  programs.nh = {
+    enable = true;
+    flake = "/home/xnp/nixos-config";
+    clean = {
+      enable = true;
+      extraArgs = "--keep-since 7d --keep 5";
+    };
   };
 }
