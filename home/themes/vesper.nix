3fac34a62a4039d064e63d82933665efe45b3916
V1PEX7 <71189313+V1PEX7@users.noreply.github.com>
2026-08-14 05:01:46 +0300

    themes: add vesper

---
+++ home/themes/vesper.nix
@@ -0,0 +1,33 @@
+{
+  bg = "#101010";
+  surface = "#161616";
+  fg = "#ffffff";
+  accent = "#ffc799";
+  hover = "#232323";
+  red = "#ff8080";
+  green = "#99ffe4";
+  yellow = "#ffc799";
+
+  shadow = "#000000";
+  calToday = "#ff8080";
+
+  hypr.shadow = "rgba(101010ee)";
+
+  term = {
+    selBg = "#343434";
+    curCursor = "#ffc799";
+
+    black = "#101010";
+    red = "#ff8080";
+    green = "#99ffe4";
+    yellow = "#ffc799";
+    blue = "#a0a0a0";
+    magenta = "#ff7300";
+    cyan = "#99ffe4";
+    white = "#ffffff";
+
+    brBlack = "#505050";
+    brYellow = "#ffcfa8";
+    brMagenta = "#ff8080";
+  };
+}
