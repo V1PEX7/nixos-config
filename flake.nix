0375b12878c9289fc5ae82c15ab73581f25c2b87
V1PEX7 <71189313+V1PEX7@users.noreply.github.com>
2026-08-19 16:03:57 +0300

    flake: add checks and formatter outputs

--- flake.nix
+++ flake.nix
@@ -14,11 +14,19 @@
     inputs@{ self, nixpkgs, ... }:
     let
       myLib = import ./lib { inherit inputs; };
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations = {
         desktop = myLib.mkHost { hostname = "desktop"; };
         laptop = myLib.mkHost { hostname = "laptop"; };
       };
+
+      formatter.${system} = pkgs.nixfmt-tree;
+
+      checks.${system} = nixpkgs.lib.mapAttrs (
+        _: host: host.config.system.build.toplevel
+      ) self.nixosConfigurations;
     };
 }
