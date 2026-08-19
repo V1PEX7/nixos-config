42de19a36d1e351b7f0375b7b82b7e4d048c347b
V1PEX7 <71189313+V1PEX7@users.noreply.github.com>
2026-08-19 18:28:29 +0300

    themes: add carbonfox

---
+++ home/themes/carbonfox.nix
@@ -0,0 +1,36 @@
+{
+  bg = "#161616";
+  surface = "#282828";
+  fg = "#f2f4f8";
+  accent = "#3ddbd9";
+  hover = "#3a3a3a";
+  red = "#ee5396";
+  green = "#25be6a";
+  yellow = "#08bdba";
+
+  shadow = "#0a0a0a";
+  calToday = "#ee5396";
+
+  term = {
+    selBg = "#2a2a2a";
+    curCursor = "#3ddbd9";
+
+    black = "#282828";
+    red = "#ee5396";
+    green = "#25be6a";
+    yellow = "#08bdba";
+    blue = "#78a9ff";
+    magenta = "#be95ff";
+    cyan = "#33b1ff";
+    white = "#dfdfe0";
+
+    brBlack = "#484848";
+    brRed = "#f16da6";
+    brGreen = "#46c880";
+    brYellow = "#2dc7c4";
+    brBlue = "#8cb6ff";
+    brMagenta = "#c8a5ff";
+    brCyan = "#52bdff";
+    brWhite = "#e4e4e5";
+  };
+}
