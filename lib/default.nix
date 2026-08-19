23695227378c78f5f063a4938ea5276c87e6fea2
V1PEX7 <71189313+V1PEX7@users.noreply.github.com>
2026-08-19 15:33:38 +0300

    home-manager: overwrite backups

--- lib/default.nix
+++ lib/default.nix
@@ -22,6 +22,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.backupFileExtension = "backup";
+          home-manager.overwriteBackup = true;
           home-manager.extraSpecialArgs = {
             inherit inputs username repoPath;
             outputs = inputs.self;
