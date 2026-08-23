1ee57a26c36e15f57a8230302fd27093329752c7
V1PEX7 <71189313+V1PEX7@users.noreply.github.com>
2026-08-24 00:12:03 +0300

    feat(gtk): inject active theme palette into gtk extra css

--- home/common.nix
+++ home/common.nix
@@ -1,9 +1,43 @@
 {
   settings,
+  theme,
   ...
 }:
 let
   s = settings;
+  t = theme;
+
+  css = ''
+    @define-color window_bg_color ${t.surface};
+    @define-color window_fg_color ${t.fg};
+    @define-color view_bg_color ${t.bg};
+    @define-color view_fg_color ${t.fg};
+    @define-color headerbar_bg_color ${t.surface};
+    @define-color headerbar_fg_color ${t.fg};
+    @define-color sidebar_bg_color ${t.surface};
+    @define-color sidebar_fg_color ${t.fg};
+    @define-color popover_bg_color ${t.hover};
+    @define-color popover_fg_color ${t.fg};
+    @define-color dialog_bg_color ${t.hover};
+    @define-color dialog_fg_color ${t.fg};
+    @define-color card_bg_color ${t.hover};
+    @define-color card_fg_color ${t.fg};
+    @define-color accent_bg_color ${t.accent};
+    @define-color accent_fg_color ${t.contrast};
+    @define-color accent_color ${t.accent};
+    @define-color destructive_bg_color ${t.red};
+    @define-color destructive_fg_color ${t.contrast};
+    @define-color destructive_color ${t.red};
+    @define-color success_bg_color ${t.green};
+    @define-color success_fg_color ${t.contrast};
+    @define-color success_color ${t.green};
+    @define-color warning_bg_color ${t.yellow};
+    @define-color warning_fg_color ${t.contrast};
+    @define-color warning_color ${t.yellow};
+    @define-color error_bg_color ${t.red};
+    @define-color error_fg_color ${t.contrast};
+    @define-color error_color ${t.red};
+  '';
 in
 {
   home.pointerCursor = {
@@ -36,6 +70,8 @@ in
       gtk-application-prefer-dark-theme = true;
       gtk-hint-font-metrics = true;
     };
+    gtk3.extraCss = css;
+    gtk4.extraCss = css;
     iconTheme = {
       name = s.iconTheme.name;
       package = s.iconTheme.package;
