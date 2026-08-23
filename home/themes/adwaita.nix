2a791424f983031371ac70fe0206f23a5d0faeb9
V1PEX7 <71189313+V1PEX7@users.noreply.github.com>
2026-08-23 23:53:12 +0300

    themes: added adwaita color theme

---
+++ home/themes/adwaita.nix
@@ -0,0 +1,35 @@
+{
+  bg = "#1d1d20";
+  surface = "#2e2e32";
+  fg = "#ffffff";
+  accent = "#3584e4";
+  hover = "#36363a";
+  red = "#ed333b";
+  green = "#33d17a";
+  yellow = "#f5c211";
+
+  contrast = "#ffffff";
+
+  term = {
+    selBg = "#3584e4";
+    curCursor = "#ffffff";
+
+    black = "#241f31";
+    red = "#c01c28";
+    green = "#2ec27e";
+    yellow = "#f5c211";
+    blue = "#1e78e4";
+    magenta = "#9841bb";
+    cyan = "#0ab9dc";
+    white = "#c0bfbc";
+
+    brBlack = "#5e5c64";
+    brRed = "#ed333b";
+    brGreen = "#57e389";
+    brYellow = "#f8e45c";
+    brBlue = "#51a1ff";
+    brMagenta = "#c061cb";
+    brCyan = "#4fd2fd";
+    brWhite = "#f6f5f4";
+  };
+}
