bb459167e6cc45ff667d7a636452409392f22ed4
V1PEX7 <71189313+V1PEX7@users.noreply.github.com>
2026-08-29 01:16:36 +0300

    feat(themes): add "quiet"

---
+++ home/themes/quiet.nix
@@ -0,0 +1,36 @@
+{
+  bg = "#1c1c1c";
+  surface = "#262626";
+  fg = "#c6c6c6";
+  accent = "#5f8faf";
+  hover = "#323232";
+  red = "#b05c5c";
+  green = "#7f9f6f";
+  yellow = "#bb9a5e";
+
+  contrast = "#1c1c1c";
+  dim = "0.45";
+
+  term = {
+    selBg = "#285577";
+    curCursor = "#c6c6c6";
+
+    black = "#2b2b2b";
+    red = "#b05c5c";
+    green = "#7f9f6f";
+    yellow = "#bb9a5e";
+    blue = "#5f8faf";
+    magenta = "#8f7fa5";
+    cyan = "#6f9f9a";
+    white = "#c6c6c6";
+
+    brBlack = "#5c5c5c";
+    brRed = "#c87878";
+    brGreen = "#96b487";
+    brYellow = "#d0b177";
+    brBlue = "#7fa8c4";
+    brMagenta = "#a897bc";
+    brCyan = "#8ab5b0";
+    brWhite = "#e2e2e2";
+  };
+}
